1b637087752f62828b159944108933dff99075c2
William Hirsch <wigsinator@gmail.com>
2019-12-05 13:31:33 -0500

    Solve Day 3 Parts 1 and 2

---
+++ src/Day3/d3p2.py
@@ -0,0 +1,21 @@
+def trace_wire(wire):
+	path = []
+	curr_pos = [0,0]
+	for inst in wire.split(','):
+		for unit in range(int(inst[1:])):
+			if inst[0] in ['U','D']:
+				curr_pos[1] += -1 if inst[0] =='D' else 1
+			else:
+				curr_pos[0] += -1 if inst[0] =='L' else 1
+			path.append(tuple(curr_pos))
+	return path
+
+f = open("d3input.txt", "r")
+wires = [trace_wire(wire) for wire in f.readlines()]
+
+intersections = set(wires[0]) & set(wires[1])
+
+
+steps = 2+ min(sum(wire.index(intersect) for wire in wires) for intersect in intersections)
+
+print(steps)
